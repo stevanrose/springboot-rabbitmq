b88d3f245d62edea240d215a8ea2b0220a92cc9b
Stevan Rose <stevan.rose@digital.homeoffice.gov.uk>
2018-05-29 16:53:02 +0100

    NOJIRA Simplified the RabbitListener

--- src/main/java/com/stevanrose/springrabbitmq/controllers/RabbitController.java
+++ src/main/java/com/stevanrose/springrabbitmq/controllers/RabbitController.java
@@ -4,10 +4,15 @@ import com.stevanrose.springrabbitmq.configuration.RabbitConfig;
 import com.stevanrose.springrabbitmq.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rabbit")
@@ -22,9 +27,10 @@ public class RabbitController {
     @ResponseStatus(HttpStatus.OK)
     public void send(@RequestBody Message message) {
         logger.info("Sending message {} to Rabbit queue", message);
-        rabbitTemplate.convertAndSend(RabbitConfig.topicExchangeName, "steve-routing", message.toString());
+        rabbitTemplate.convertAndSend(RabbitConfig.topicExchangeName, RabbitConfig.routingKey, message.toString());
     }
 
+    @RabbitListener(queues = RabbitConfig.queueName)
     private void handleMessage(String message) {
         logger.info("Received message {} from Rabbit Queue", message);
     }
